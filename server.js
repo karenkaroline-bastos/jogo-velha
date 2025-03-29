cc6e1b10d3ec3c85f8b0d2242567eccb9cb6f502
Karen Karoline Bastos <137943069+karenkaroline-bastos@users.noreply.github.com>
2025-03-29 14:52:24 +0000

    feat: INserindo o complemento da tela login.html

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const req = require("express/lib/request");
 
 const app = express();
 const prisma = new PrismaClient();
 
 const PORT = 8080;
 
-
+app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/api", (req, res) => {
@@ -35,6 +36,28 @@ app.post("/api/v1/usuarios", async (req, res) =>{
     res.status(201).json(usuario);
 });
 
+
+app.post('/api/v1/login', async(req, res) => {
+    const {email, senha }= req.body;
+    const usuario = await prisma.usuario.findUnique({where:{email, senha}});
+    console.log(usuario); 
+    if(usuario == null){
+        return res.status(401).json({error:"Credenciais Inválidas"});
+    }
+    else{
+        return res.status(200).json(usuario);
+    }
+})
+
+    /*
+    200 - Deu certo
+    400 - Erro de validação
+    401 - Não autorizado
+    403 - Eu sei que você está tentando fazer, mas não pode
+    404 - Não encontrado
+    500 - Erro interno do servidor
+
+    */
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
