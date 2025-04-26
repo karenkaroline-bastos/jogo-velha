ebe96227fa1ea682d9f1900c5c29ceae1e163721
Karen Karoline Bastos <137943069+karenkaroline-bastos@users.noreply.github.com>
2025-04-25 21:27:40 -0300

    feat: atualizar arquivos

--- server.js
+++ server.js
@@ -14,50 +14,104 @@ app.get("/api", (req, res) => {
     res.json({ message: "API funcionando corretamente!" });
 });
 
-app.post("/api/v1/usuarios", async (req, res) =>{
-    const   {
-                nome, 
-                email, 
-                senha, 
-                username, 
-                avatar
-            } = req.body;
+app.post("/api/v1/usuarios", async (req, res) => {
+    const {
+        nome,
+        email,
+        senha,
+        username,
+        avatar
+    } = req.body;
 
     const usuario = await prisma.usuario.create({
         data: {
-            nome, 
-            email, 
-            senha, 
-            username, 
+            nome,
+            email,
+            senha,
+            username,
             avatar
-        }        
+        }
     });
 
     res.status(201).json(usuario);
 });
 
+// app.post("/api/v1/conexao", async(data, response))
+app.post("/api/v1/conexao", async (req, res) => {
+    const {
+        id_usuario_a,
+        id_usuario_b
+    } = req.body;
 
-app.post('/api/v1/login', async(req, res) => {
-    const {email, senha }= req.body;
-    const usuario = await prisma.usuario.findUnique({where:{email, senha}});
-    console.log(usuario); 
-    if(usuario == null){
-        return res.status(401).json({error:"Credenciais Inválidas"});
+    const conexao = await prisma.conexao.create({
+        data: {
+            id_usuario_a,
+            id_usuario_b
+        }
+    });
+
+    console.log(conexao);
+    res.status(201).json(conexao);
+})
+
+app.get("/api/v1/conexao", async (req, res) => {
+    const conexao = await prisma.conexao.findMany();
+    res.status(200).json(conexao);
+})
+// data: JSON.stringify({ id_partida, id_usuario, area_jogada,peca_do_jogador }),
+
+app.post("/api/v1/partidaHistorico", async (req, res) => {
+    const {
+        id_partida,
+        id_usuario,
+        area_jogada,
+        peca_do_jogador
+    } = req.body;
+
+    const ultimaJogada = await prisma.partida_historico.findFirst({
+        where: { id_partida },
+        orderBy: { id: 'desc' },
+    });
+
+    const localOcupado = await prisma.partida_historico.findFirst({
+        where: {
+            area_jogada,
+            id_partida
+        }
+    })
+
+    if(ultimaJogada['id_usuario'] == id_usuario){
+        res.status(422).json('Não é sua vez de jogar');
+    }
+    else if(localOcupado != null){
+        res.status(422).json('Posição ja ocupada');
     }
     else{
+        const partida_historico = await prisma.partida_historico.create({
+            data: {
+                id_partida,
+                id_usuario,
+                area_jogada,
+                peca_do_jogador
+            }
+        });
+
+        res.status(201).json(partida_historico);
+    }
+});
+
+app.post('/api/v1/login', async (req, res) => {
+    const { email, senha } = req.body;
+    const usuario = await prisma.usuario.findUnique({ where: { email, senha } });
+
+    if (usuario == null) {
+        return res.status(401).json({ error: "Credenciais Inválidas" });
+    }
+    else {
         return res.status(200).json(usuario);
     }
 })
 
-    /*
-    200 - Deu certo
-    400 - Erro de validação
-    401 - Não autorizado
-    403 - Eu sei que você está tentando fazer, mas não pode
-    404 - Não encontrado
-    500 - Erro interno do servidor
-
-    */
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
